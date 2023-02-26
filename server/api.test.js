5c8a7c449a7918c10765fad2487b79cfbbf5bf87
petrarubic <petra@caelor.com>
2023-02-26 13:29:42 +0100

    write test for encode route

--- server/api.test.js
+++ server/api.test.js
@@ -1,2 +1,16 @@
 const request = require("supertest");
 const baseURL = "http://localhost:3000";
+
+describe("Encode given string", () => {
+  const input = { text: "XXXXYYYZZ" };
+  beforeAll(async () => {
+    await request(baseURL).post("/encode").send(input);
+  });
+  it("should return encoded string", async () => {
+    const response = await request(baseURL)
+      .post("/encode")
+      .send(encoder(input));
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toBe("X4Y3Z2");
+  });
+});
